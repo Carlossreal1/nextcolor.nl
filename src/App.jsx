7737c0965348417f98ded94b309417cb38f8bbe1
Carlossreal1 <joseccrodrigues@hotmail.com>
2025-11-02 16:47:28 +0100

    photo

--- src/App.jsx
+++ src/App.jsx
@@ -48,9 +48,10 @@ export default function App() {
             { src: "/amario1.jpeg", alt: "Woning interieur" },
             { src: "/amario4.jpeg", alt: "Woning interieur" },
             { src: "/amario5.jpeg", alt: "Woning interieur" },
+            { src: "/porta5.jpeg", alt: "Woning interieur" },
             { src: "/winkel_pauw12.jpeg", alt: "Buitenmuur schilderwerk" },
             { src: "/winkel_pauw13.jpeg", alt: "Buitenmuur schilderwerk" },
-            { src: "/porta5.jpeg", alt: "Buitenmuur schilderwerk" },
+
             { src: "/trap4.jpeg", alt: "Trap renovatie" },
             { src: "/trap2.jpeg", alt: "Trap renovatie" },
             { src: "/trap3.jpeg", alt: "Trap renovatie" },
